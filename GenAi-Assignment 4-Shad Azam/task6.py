2e96a62e4b804801f9fd33eebc26039d29450639
Shad Azam <azamshad08@gmail.com>
2026-05-20 10:11:50 +0530

    update

--- GenAi-Assignment 4-Shad Azam/task6.py
+++ GenAi-Assignment 4-Shad Azam/task6.py
@@ -1,1 +1,16 @@
 # Task 6: Read File Safely (Error Handling Inside File Handling Only)
+
+# import OS
+import os
+
+# get the file name from user
+given_input = input("Enter the file name: ")
+
+# check file name exits or not using os.path.exits()
+if os.path.exists(given_input):
+    # open file in read mode and print
+    with open(given_input, 'r') as f:
+        read_content = f.read()
+        print(read_content)
+else:
+    print("File Not Found Please check the filename")
--- GenAi-Assignment 4-Shad Azam/task7.py
+++ GenAi-Assignment 4-Shad Azam/task7.py
@@ -1,1 +1,36 @@
 # Task 7: Mini Project- Export Price List
+
+# Create Dicitionary key value pair
+price = {
+    "Mouse": 500,
+    "Keyboard": 800,
+    "Monitor": 7000,
+    "Pendrive": 400,
+    "Camera": 5000
+}
+
+# ask the user input
+discount_percentage = int(input("Enter the discount percentage: "))
+
+total_items = len(price)
+toata_discount_price = 0
+
+# write the file with write mode
+with open('discounted_report.txt', 'w') as f:
+    f.write("Product | Original Price | Discounted Price\n")
+    # loop the price and product and calculte the discount price and write in file
+    for product, orignalPrice in price.items():
+        discount_price = orignalPrice - ((orignalPrice * discount_percentage) / 100)
+        toata_discount_price += discount_price
+        f.write(f"{product} | {orignalPrice} | {discount_price:.2f}\n")
+
+    # Claculate average
+    average_discount_price = toata_discount_price / total_items
+    f.write(f"Total Items: {total_items}\n")
+    f.write(f"Average Discounted Price: {average_discount_price}")
+
+
+# read file and print the content
+with open("discounted_report.txt", 'r') as f:
+    content_file = f.read()
+    print(content_file)
